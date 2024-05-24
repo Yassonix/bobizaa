61cf09f4076accd37f66c594cdb5ab501ef52079
Yassonix <yassineamhamdi977@gmail.com>
2024-05-24 09:15:20 +0100

    Update tz.js

--- plugins/tz.js
+++ plugins/tz.js
@@ -9,7 +9,7 @@ const handler = async (m, {conn}) => {
 
 ▢ morocco     : ${tzAF}
   ${String.fromCharCode(8206).repeat(850)}
-  ▢ instagram.com/noureddine_ouafy`}, {quoted: m});
+  ▢ yassine amhamdi`}, {quoted: m});
   };
 handler.help = ["tz"]
 handler.tags = ["infobot"]
