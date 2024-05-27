739f73096d5970a733cf3e3c4b33722e38bf0d4c
Yassonix <yassineamhamdi977@gmail.com>
2024-05-27 17:07:04 +0100

    Create animiw.js

---
+++ plugins/animiw.js
@@ -0,0 +1,55 @@
+{
+  "title": "Vampire Dormitory Subtitle Indonesia",
+  "image": "https://otakudesu.cloud/wp-content/uploads/2024/04/Vampire-Dormitory.jpg",
+  "judul": "Judul: Vampire Dormitory",
+  "japanese": "Japanese: ヴァンパイア男子寮",
+  "skor": "Skor: 6.24",
+  "produser": "Produser: Dentsu, Kodansha, Avex Pictures, Crunchyroll",
+  "tipe": "Tipe: TV",
+  "status": "Status: Ongoing",
+  "total_episode": "Total Episode: 12",
+  "durasi": "Durasi: 23 min. per ep.",
+  "tanggal_rilis": "Tanggal Rilis: Apr 07, 2024",
+  "studio": "Studio: Studio Blanc.",
+  "genre": "Genre: Romance, Shoujo, Supernatural, Vampire",
+  "sinopsis": "Mito, gadis cantik namun memiliki nasib malang. Pasalnya, ia telah kehilangan kedua orang tuanya dan kini ditelantarkan oleh kerabatnya dan terpaksa diusir dari tempatnya bekerja. Hal ini dikarenakan Mito yang menyamar sebagai laki-laki.Dalam kondisi yang tak karuan, ia dipertemukan oleh Ruka yang merupakan seorang vampir. Melihat kondisi Mito, Ruka memutuskan untuk membawanya dan tinggal bersamanya. Namun, tentu Mito harus membayar imbalan berupa darahnya akan dihisap oleh Ruka.Beginilah kisah Mito yang harus menyamar sebagai laki-laki dikepungan siswa cowok.",
+  "batch": "https://otakudesu.cloud/episode/vprd-episode-7-sub-indo/",
+  "episode": [
+    {
+      "judul": "Vampire Dormitory Episode 7 Subtitle Indonesia",
+      "link": "https://otakudesu.cloud/episode/vprd-episode-7-sub-indo/",
+      "upload": "27 Mei,2024"
+    },
+    {
+      "judul": "Vampire Dormitory Episode 6 Subtitle Indonesia",
+      "link": "https://otakudesu.cloud/episode/vprd-episode-6-sub-indo/",
+      "upload": "13 Mei,2024"
+    },
+    {
+      "judul": "Vampire Dormitory Episode 5 Subtitle Indonesia",
+      "link": "https://otakudesu.cloud/episode/vprd-episode-5-sub-indo/",
+      "upload": "6 Mei,2024"
+    },
+    {
+      "judul": "Vampire Dormitory Episode 4 Subtitle Indonesia",
+      "link": "https://otakudesu.cloud/episode/vprd-episode-4-sub-indo/",
+      "upload": "29 April,2024"
+    },
+    {
+      "judul": "Vampire Dormitory Episode 3 Subtitle Indonesia",
+      "link": "https://otakudesu.cloud/episode/vprd-episode-3-sub-indo/",
+      "upload": "29 April,2024"
+    },
+    {
+      "judul": "Vampire Dormitory Episode 2 Subtitle Indonesia",
+      "link": "https://otakudesu.cloud/episode/vprd-episode-2-sub-indo/",
+      "upload": "15 April,2024"
+    },
+    {
+      "judul": "Vampire Dormitory Episode 1 Subtitle Indonesia",
+      "link": "https://otakudesu.cloud/episode/vprd-episode-1-sub-indo/",
+      "upload": "13 April,2024"
+    }
+  ],
+  "download": []
+}
