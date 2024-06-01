ba27ab4146eefc133f7af0d32499665423ef2f75
Yassonix <yassineamhamdi977@gmail.com>
2024-06-01 12:17:13 +0100

    Update channel.js

--- plugins/channel.js
+++ plugins/channel.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 
-m.reply(`*قناتي على الواتساب:*\n
-*https://whatsapp.com/channel/0029VaLopUxFnSz7D2CH9S0P*
+m.reply(`*قناتي على انستغرام:*\n
+*https://www.instagram.com/gtx_33_sr?igsh=bG4xZDVmaGp5am03*
 *تابعني هناك♥*`)
 }
 handler.help = ['channel']
